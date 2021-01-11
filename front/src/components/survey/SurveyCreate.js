cca9b30848a768ce19924e087a97d81ad68e1b52
sunday00 <kind65505256@gmail.com>
2021-01-11 21:54:15 +0900

    create ing

--- front/src/components/survey/SurveyCreate.js
+++ front/src/components/survey/SurveyCreate.js
@@ -1,65 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { makeStyles, withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+
 import Typography from '@material-ui/core/Typography';
-import TextField from '@material-ui/core/TextField';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Container from '@material-ui/core/Container';
-import Box from '@material-ui/core/Box';
+
+import { useSurveyStyle, CssTextField } from '../../lib/styles/mainStyle';
 
 // import { mainStyle } from '../lib/styles/mainStyle';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    backgroundColor: theme.palette.primary.main,
-    padding: theme.spacing(4),
-    alignSelf: 'center',
-  },
-  paper: {
-    padding: theme.spacing(2),
-    textAlign: 'center',
-  },
-  title: {
-    // fontFamily: 'Black Han Sans',
-    fontFamily: 'Gugi',
-    color: '#fff',
-    // font-family: 'Jua', sans-serif;
-    // font-family: 'Nixie One', cursive;
-    // font-family: 'Noto Sans KR', sans-serif;
-    // font-family: 'Teko', sans-serif;
-  },
-  button: {
-    marginTop: theme.spacing(2),
-  },
-}));
-
-const CssTextField = withStyles((theme) => ({
-  root: {
-    '& label.MuiFormLabel-root.Mui-focused': {
-      color: theme.palette.primary.contrastText,
-    },
-    '& label.MuiFormLabel-root': {
-      color: theme.palette.primary.superLight,
-    },
-    '& .MuiOutlinedInput-root': {
-      '& fieldset': {
-        borderColor: theme.palette.primary.superLight,
-        color: theme.palette.primary.superLight,
-      },
-      '&:hover fieldset': {
-        borderColor: 'yellow',
-      },
-      '&.Mui-focused fieldset': {
-        borderColor: theme.palette.primary.contrastText,
-      },
-    },
-  },
-}))(TextField);
-
 const SurveyCreate = () => {
-  const classes = useStyles();
+  const classes = useSurveyStyle();
+  const [selectedDate, setSelectedDate] = useState(
+    new Date('2014-08-18T21:11:54'),
+  );
+  const handleDateChange = (date) => {
+    setSelectedDate(date);
+  };
   return (
     <Container component="main" maxWidth="xs" className={classes.root}>
       <CssBaseline />
@@ -83,27 +41,45 @@ const SurveyCreate = () => {
             variant="outlined"
             margin="normal"
             required
+            multiline
             fullWidth
-            id="Group"
-            label="Group"
-            name="Group"
+            id="description"
+            label="설명"
+            name="description"
+            rows="3"
             // onChange={(e) => handleSubInfoChange(e, 'group')}
           />
-          <CssTextField
-            variant="outlined"
-            margin="normal"
-            fullWidth
-            name="SubGroup"
-            label="SubGroup"
-            id="SubGroup"
-            // onChange={(e) => handleSubInfoChange(e, 'subGroup')}
-          />
+          <div
+            className="MuiFormControl-marginNormal"
+            style={{ display: 'flex', justifyContent: 'space-between' }}
+          >
+            <CssTextField
+              variant="outlined"
+              id="date"
+              label="start"
+              type="date"
+              defaultValue="2017-05-24"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+            <CssTextField
+              variant="outlined"
+              id="date"
+              label="start"
+              type="date"
+              defaultValue="2017-05-24"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </div>
 
           <Button
             type="submit"
             fullWidth
             variant="contained"
-            color="primary"
+            color="secondary"
             className={classes.button}
           >
             Next
--- front/src/lib/styles/mainStyle.js
+++ front/src/lib/styles/mainStyle.js
@@ -1,12 +1,64 @@
-import { makeStyles, createStyles } from '@material-ui/core/styles';
+import { makeStyles, createStyles, withStyles } from '@material-ui/core/styles';
+import TextField from '@material-ui/core/TextField';
 
 export const mainStyle = makeStyles((theme) =>
   createStyles({
     root: {
-      backgroundColor: theme.palette.primary.main,
+      backgroundColor: theme.palette.primary.light,
       color: theme.palette.primary.contrastText,
       display: 'flex',
       flex: 1,
     },
   }),
 );
+
+export const useSurveyStyle = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+    backgroundColor: theme.palette.primary.main,
+    padding: theme.spacing(4),
+    alignSelf: 'center',
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+  },
+  title: {
+    // fontFamily: 'Black Han Sans',
+    fontFamily: 'Gugi',
+    color: '#fff',
+    // font-family: 'Jua', sans-serif;
+    // font-family: 'Nixie One', cursive;
+    // font-family: 'Noto Sans KR', sans-serif;
+    // font-family: 'Teko', sans-serif;
+  },
+  button: {
+    marginTop: theme.spacing(2),
+  },
+}));
+
+export const CssTextField = withStyles((theme) => ({
+  root: {
+    '& .MuiInputBase-input ': {
+      color: theme.palette.primary.contrastText,
+    },
+    '& label.MuiFormLabel-root.Mui-focused': {
+      color: theme.palette.primary.contrastText,
+    },
+    '& label.MuiFormLabel-root': {
+      color: theme.palette.primary.superLight,
+    },
+    '& .MuiOutlinedInput-root': {
+      '& fieldset': {
+        borderColor: theme.palette.primary.superLight,
+        color: theme.palette.primary.superLight,
+      },
+      '&:hover fieldset': {
+        borderColor: 'yellow',
+      },
+      '&.Mui-focused fieldset': {
+        borderColor: theme.palette.primary.contrastText,
+      },
+    },
+  },
+}))(TextField);
