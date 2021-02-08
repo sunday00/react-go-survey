d39cd5eeafc3e8cca565f7d6335424fcd923a839
sunday00 <kind65505256@gmail.com>
2021-02-08 22:47:41 +0900

    decide progress and make main page

--- front/src/components/survey/ReadContainer.js
+++ front/src/components/survey/ReadContainer.js
@@ -1,17 +1,38 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 import { read } from '../../modules/survey';
+import ReadMain from './ReadMain';
+
+import CircularProgress from '@material-ui/core/CircularProgress';
+import Container from '@material-ui/core/Container';
+import CssBaseline from '@material-ui/core/CssBaseline';
+
+import { useSurveyStyle, CssTextField } from '../../lib/styles/mainStyle';
 
 const ReadContainer = ({ match }) => {
   const surveyNo = match.params.surveyNo;
+
+  const classes = useSurveyStyle();
   const dispatch = useDispatch();
   const survey = useSelector((state) => state.survey);
+
+  const [page, setPage] = useState(0);
 
   useEffect(() => {
     if (!survey.main.title) dispatch(read(surveyNo));
   }, [dispatch, survey.main.title, surveyNo]);
 
-  return <div>read</div>;
+  return (
+    <Container component="main" maxWidth="xs" className={classes.root}>
+      <CssBaseline />
+      <div className={classes.paper}>
+        {!survey.main.title && <CircularProgress color="secondary" />}
+        {survey.main.title && page === 0 && (
+          <ReadMain title={survey.main.title} description={survey.main.description} />
+        )}
+      </div>
+    </Container>
+  );
 };
 
 export default ReadContainer;
---
+++ front/src/components/survey/ReadMain.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const ReadMain = ({ title, description }) => {
+  //TODO:: make buttons : go back to list / start
+  return (
+    <div>
+      <h2>{title}</h2>
+      <p>{description}</p>
+    </div>
+  );
+};
+
+export default ReadMain;
